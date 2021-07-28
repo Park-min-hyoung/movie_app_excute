ef427208f78428d19f4e37926e96c0abb8adf5ab
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-28 20:26:23 +0900

    JSX 및 component 정보 전달과 그 정보 사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <Potato />
+      <Food fav="1" />
+      <Food fav="2" />
+      <Food fav="3" />
+      <Food fav="4" />
+      <Food fav="5" />
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,5 +0,0 @@
-function Potato() {
-    return <h1>I love PT</h1>;
-}
-
-export default Potato;
