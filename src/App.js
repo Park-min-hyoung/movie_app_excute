00a1f5959847ff86355e3848d53b9bd97557f0d1
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-28 21:27:33 +0900

    state 기본 개념

--- src/App.js
+++ src/App.js
@@ -1,67 +1,24 @@
 import React from 'react';
-import propTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: 
-        "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "bulgogi",
-    image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLhTuEcDaqRHCSbBTW7Ynte4mNsKsiag2h6w&usqp=CAU",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "kimbap",
-    image:
-        "https://recipe1.ezmember.co.kr/cache/recipe/2018/06/05/5ab3fafe7e9f2f5a7cab7c6d12abdefb1.jpg",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "samgyetang",
-    image:
-        "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    rating: 4.7
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
   }
-]
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}</h4>
-      <img src = {picture} />
-    </div>
-  ) 
-}
-
-Food.propTypes = {
-  // Food function componet의 name props는 string 자료형만 넘어오는 것을 허용한다.
-  name: propTypes.string.isRequired, 
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food 
-          key={dish.id} 
-          name={dish.name} 
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  minus = () => {
+    console.log("minus");
+  }
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
